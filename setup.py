1f6a82298a3e3224ccc17da158bb558bccd6ed42
Connor Ferster <CFerster@rjc.local>
2020-04-03 19:51:07 -0700

    Added IPython render extension

--- setup.py
+++ setup.py
@@ -18,10 +18,12 @@ URL = 'https://github.com/connorferster/handcalcs'
 EMAIL = 'connorferster@gmail.com'
 AUTHOR = 'Connor Ferster'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
+    'jinja2',
+    'pyparsing'
     # 'requests', 'maya', 'records',
 ]
 
