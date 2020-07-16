8278ef0b9b4efb372b01f462d6493d25c6301e47
Connor Ferster <CFerster@rjc.ca>
2020-07-16 09:10:54 -0700

    Added functionality for #Short comment tag; grammatical corrections to README.md

--- handcalcs/__init__.py
+++ handcalcs/__init__.py
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 from handcalcs.handcalcs import handcalc
 __all__ = ["handcalc"]
 
