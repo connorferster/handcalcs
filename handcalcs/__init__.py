22708193a3e92ee77778747e3fa50f4c0c103f0f
Connor Ferster <CFerster@rjc.ca>
2020-08-25 21:51:18 -0700

    Updated version

--- handcalcs/__init__.py
+++ handcalcs/__init__.py
@@ -12,7 +12,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 from handcalcs.decorator import handcalc
 from handcalcs.install_templates import install_html, install_latex
 __all__ = ["handcalc"]
