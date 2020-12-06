7a750dc99912c230b9a8e210e9a746b4cc6d54d5
Connor Ferster <connorferster@gmail.com>
2020-12-05 22:17:36 -0800

    tests: updated cell_10.py to include tests for numeric lines, nested log functions, and floor func

--- test_handcalcs/cell_10.py
+++ test_handcalcs/cell_10.py
@@ -1,5 +1,8 @@
-from math import sin
+from math import floor, sin, log, log2, log10, sqrt
 
-mu = 45 + sin(34 + 2)/2
+mu = 45 + sin(34 + 2)/2 # Comment
+tau = sin(log2(log(3, 9)))
+eta = sqrt(1 / log10(6) / log(32))
+kappa = floor(23/4.5) # Last comment
 
 calc_results = globals()
