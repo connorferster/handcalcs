e086370e43eb1ea8e9ba0829cf75e4cefec7f45b
Dara Adib <daradib@ocf.berkeley.edu>
2021-12-03 20:33:06 -0500

    Add test for exporter parent class attribute

--- test_handcalcs/test_handcalcs_file.py
+++ test_handcalcs/test_handcalcs_file.py
@@ -282,16 +282,22 @@ def test_handcalcs3():
 def test_latex_exporter():
     exporter = LatexHideInputExporter()
     assert exporter.exclude_input == True
+    parent = exporter.__class__.__bases__[0]()
+    assert parent.exclude_input == False
 
 
 def test_pdf_exporter():
     exporter = PDFHideInputExporter()
     assert exporter.exclude_input == True
+    parent = exporter.__class__.__bases__[0]()
+    assert parent.exclude_input == False
 
 
 def test_html_exporter():
     exporter = HTMLHideInputExporter()
     assert exporter.exclude_input == True
+    parent = exporter.__class__.__bases__[0]()
+    assert parent.exclude_input == False
 
 # Test expected exceptions
 
